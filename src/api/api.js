eacc0bbaed83e757e6be6b08439fe264b25bef3a
bishal16 <mahathirbisal@gmail.com>
2024-07-18 13:22:44 +0600

    added api client axios

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+// src/api.js
+
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:8080', // Backend base URL
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// User APIs
+export const createUser = (userData) => apiClient.post('/users', userData);
+export const getUserById = (id) => apiClient.get(`/users/${id}`);
+export const getUserByUsername = (username) => apiClient.get(`/users/username/${username}`);
+
+// Contact APIs
+export const addContact = (contactData) => apiClient.post('/contacts', contactData);
+
+// Message APIs
+export const sendMessage = (messageData) => apiClient.post('/messages', messageData);
+export const getMessages = (senderId, recipientId) => apiClient.get(`/messages/${senderId}/${recipientId}`);
+
+export default apiClient;
